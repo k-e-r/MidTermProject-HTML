e64ed6f14da9aab8d35718631a4405fb909bbed0
k-e-r <damajonboua@gmail.com>
2021-12-14 10:18:07 -0800

    add click function for hero images

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
-const fetchData = async (search) => {
-  const response = await axios.get('https://api.annict.com/v1/works', {
-    params: {
-      access_token: 'M6Vyw8ESfMMLQy7sAlfRdTeVhSuFvTh2GXTHw2nd8_A',
-      filter_title: search,
-      sort_id: 'desc',
-    },
-  });
-
-  console.log(response.data);
-  return search === '' ? '' : response.data;
-};
-
-const createAutoComplete = () => {
+createAutoComplete = () => {
   document.querySelector('#autocomplete').innerHTML = `
-    <label>Animation name is 
-    <input class="input" placeholder="ラブひな" />
-    </label>
+    <label><input class="input" placeholder="ラブひな" /></label>
   `;
-
   const input = document.querySelector('#autocomplete').querySelector('input');
-  const results = document.querySelector('#contents').querySelector('.results');
-
   const onInput = async (event) => {
-    const lists = await fetchData(event.target.value);
-
+    const lists = await searchTitle(event.target.value);
+    const results = document
+      .querySelector('#contents')
+      .querySelector('.results');
     if (lists.total_count === 0) {
       const option = document.createElement('a');
+      results.innerHTML = '';
       option.innerHTML = `
         <div class="undefined">
           <p>"${event.target.value}" is UNDEFINED</p>
         </div>
       `;
-
+      let slides = document.getElementsByClassName('switchSlides');
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+      }
       results.appendChild(option);
-    } else if (lists != []) {
-      const items = lists.works;
-      results.innerHTML = '';
-      for (let item of items) {
+    } else outputResult(lists);
+  };
+  input.addEventListener('input', debounce(onInput, 500));
+};
+
+outputResult = (lists) => {
+  const results = document.querySelector('#contents').querySelector('.results');
+  if (lists != []) {
+    const items = lists.works;
+    results.innerHTML = '';
+    for (let item of items) {
+      if (item.season_name !== '' && item.season_name !== undefined) {
         const option = document.createElement('a');
         const image =
           item.images.recommended_url === ''
             ? 'https://placehold.jp/400x250.png'
             : item.images.recommended_url;
         const url =
-          item.official_site_url == ''
+          item.official_site_url === ''
             ? '-'
             : '<a href="' +
               item.official_site_url +
@@ -60,12 +56,67 @@ const createAutoComplete = () => {
             <p>Official site URL: ${url}</p>
           </div>
         `;
-
+        let slides = document.getElementsByClassName('switchSlides');
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = 'none';
+        }
         results.appendChild(option);
       }
-    } else results.innerHTML = '';
-  };
-  input.addEventListener('input', debounce(onInput, 500));
+    }
+  } else {
+    results.innerHTML = '';
+    let slides = document.getElementsByClassName('switchSlides');
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'block';
+    }
+  }
+};
+
+createHeroImage = async () => {
+  const slide = document.querySelector('#slideshow-container');
+  const lists = await searchSeason('2021-winter');
+
+  if (lists != []) {
+    const items = lists.works;
+    slide.innerHTML = '';
+    for (let item of items) {
+      if (item.images.recommended_url !== '') {
+        const option = document.createElement('a');
+        option.setAttribute('data-id', item.title);
+        option.onclick = async function () {
+          const input = document
+            .querySelector('#autocomplete')
+            .querySelector('input');
+          input.value = this.dataset.id;
+          const lists = await searchTitle(this.dataset.id);
+          outputResult(lists);
+        };
+        option.innerHTML = `
+          <div class="showSlides">
+            <img class="switchSlides" src="${item.images.recommended_url}" />
+          </div>
+        `;
+        slide.appendChild(option);
+      }
+    }
+  } else slide.innerHTML = '';
+  showSlides();
+};
+
+let slideIndex = 0;
+showSlides = () => {
+  let i;
+  let slides = document.getElementsByClassName('showSlides');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showSlides, 8000);
 };
 
 createAutoComplete();
+createHeroImage();
--- utils.js
+++ utils.js
@@ -9,3 +9,29 @@ const debounce = (func, delay = 1000) => {
     }, delay);
   };
 };
+
+const searchTitle = async (title) => {
+  const response = await axios.get('https://api.annict.com/v1/works', {
+    params: {
+      access_token: 'M6Vyw8ESfMMLQy7sAlfRdTeVhSuFvTh2GXTHw2nd8_A',
+      filter_title: title,
+      sort_season: 'desc',
+    },
+  });
+
+  console.log(response.data);
+  return title === '' ? '' : response.data;
+};
+
+const searchSeason = async (season) => {
+  const response = await axios.get('https://api.annict.com/v1/works', {
+    params: {
+      access_token: 'M6Vyw8ESfMMLQy7sAlfRdTeVhSuFvTh2GXTHw2nd8_A',
+      filter_season: season,
+      sort_watchers_count: 'desc',
+    },
+  });
+
+  console.log(response.data);
+  return season === '' ? '' : response.data;
+};
