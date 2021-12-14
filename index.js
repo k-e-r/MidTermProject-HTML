899d22102365b39c3bfb67cfa6659b83352c8b25
k-e-r <damajonboua@gmail.com>
2021-12-13 22:31:38 -0800

    modify undefined url

--- index.js
+++ index.js
@@ -22,29 +22,9 @@ const createAutoComplete = () => {
   const results = document.querySelector('#contents').querySelector('.results');
 
   const onInput = async (event) => {
-    const items = await fetchData(event.target.value);
+    const lists = await fetchData(event.target.value);
 
-    results.innerHTML = '';
-    for (let item of items.works) {
-      const option = document.createElement('a');
-      const image =
-        item.images.recommended_url === ''
-          ? 'https://placehold.jp/400x250.png'
-          : item.images.recommended_url;
-
-      option.innerHTML = `
-        <img src="${image}" />
-        <div class="contents">
-          <p>Title : ${item.title}</p>
-          <p>Data : ${item.season_name_text}</p>
-          <p>Release media : ${item.media_text}</p>
-          <p>Official site URL: <a href="${item.official_site_url}" target="_blank">${item.official_site_url}</a></p>
-        </div>
-      `;
-
-      results.appendChild(option);
-    }
-    if (items.total_count === 0) {
+    if (lists.total_count === 0) {
       const option = document.createElement('a');
       option.innerHTML = `
         <div class="undefined">
@@ -53,7 +33,37 @@ const createAutoComplete = () => {
       `;
 
       results.appendChild(option);
-    }
+    } else if (lists != []) {
+      const items = lists.works;
+      results.innerHTML = '';
+      for (let item of items) {
+        const option = document.createElement('a');
+        const image =
+          item.images.recommended_url === ''
+            ? 'https://placehold.jp/400x250.png'
+            : item.images.recommended_url;
+        const url =
+          item.official_site_url === ''
+            ? '-'
+            : '<a href="' +
+              item.official_site_url +
+              '" target="_blank">' +
+              item.official_site_url +
+              '</a>';
+
+        option.innerHTML = `
+          <img src="${image}" />
+          <div class="contents">
+            <p>Title : ${item.title}</p>
+            <p>Data : ${item.season_name_text}</p>
+            <p>Release media : ${item.media_text}</p>
+            <p>Official site URL: ${url}</p>
+          </div>
+        `;
+
+        results.appendChild(option);
+      }
+    } else results.innerHTML = '';
   };
   input.addEventListener('input', debounce(onInput, 500));
 };
